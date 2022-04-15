19d900a10187246b835cce1fc4017c892bf9211f
Elguncode <elgunmb@code.edu.az>
2022-04-15 20:21:27 +0400

    tapsiriq

--- task4/Program.cs
+++ task4/Program.cs
@@ -8,7 +8,27 @@ namespace task4
         {
             // 1-1000 qeder ederlerin icerisinde 
             //11 - e bolunub reqemleri cemi 11 - den boyuk olan 11 - ci eded hansidir?
+            int numbers;
+            int temp;
+            int sum;
+            for(numbers= 1; numbers < 1000; numbers++)
+            {
+                temp = numbers;
+                sum = 0;
+                if (temp % 11 == 0)
+                {
+                    while (temp > 0)
+                    {
+                        sum = sum + (temp % 10);
+                        temp /= 10;
+                    }
+                    if (sum > 11)
+                    {
+                        Console.WriteLine(numbers);
 
+                    }   
+                }
+            }    
         }
     }
 }
---
+++ task6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Her hansi bir eded daxil et.
+            //Bu ededin daxilinden 5 ve 7 reqemlerini legv et.
+            //Neticenin I ve Sonuncu reqemlerini deyishib cap et.
+            //Alinan cavabin icerisinde nece dene tek reqem oldugunu yaz.
+           
+
+
+        }
+    }
+}
